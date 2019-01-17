5a9441eda67d35b5ba56d66012130807a004a0ed
Vince <cakewithsteak@gmail.com>
2019-01-17 19:56:55 +0100

    Added connections between cells properly

--- network.py
+++ network.py
@@ -10,47 +10,85 @@ class AutoDeeplab(nn.Module):
         :param cell: The cell class to use.
         """
         super(AutoDeeplab, self).__init__()
+        self.upsample_at_end = upsample_at_end
+        self.cells = []
 
-        cells = []
-        channels = in_channels
+        self.initial_stem = nn.Sequential(
+            nn.Conv2d(in_channels, 64, 3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            activation()
+        ).cuda()
+
+        self.cells.append(nn.Sequential(
+                nn.Conv2d(64, 64, 3, padding=1),
+                nn.BatchNorm2d(64),
+                activation()
+        ))
+
+        self.cells.append(nn.Sequential(
+            nn.Conv2d(64, 128, 3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            activation()
+        ))
+
+
+        #self.stem = nn.Sequential(
+        #    nn.Conv2d(in_channels, 64, 3, stride=2, padding=1),
+        #    nn.Conv2d(64, 64, 3, padding=1),
+        #    nn.Conv2d(64, 128, 3, stride=2, padding=1),
+        #).cuda()
+
+        channels = 128
+        assert layout[0] == 2
         for i, depth in enumerate(layout):
-            layer = i + 1
-            size = 2 ** depth
-            if i != 0:
-                prev_depth = layout[i - 1]
-                assert abs(depth - prev_depth) <= 1
-                if prev_depth < depth:
+            layer = [cell(channels, channels)]
+            # todo dilation?
+
+            if i != len(layout) - 1:
+                next_depth = layout[i + 1]
+                assert abs(depth - next_depth) <= 1
+                if next_depth > depth:
                     # Downsampling
-                    cells.append(nn.Conv2d(channels, channels * 2, 3, stride=2, padding=1))
+                    layer.append(nn.Conv2d(channels, channels * 2, 3, stride=2, padding=1))
                     channels = channels * 2
-                elif prev_depth > depth:
+                elif next_depth < depth:
                     # Upsampling
-                    cells.append(nn.Upsample(scale_factor=2, mode="bilinear"))
-                    cells.append(nn.Conv2d(channels, channels // 2, 1))
+                    layer.append(nn.Upsample(scale_factor=2, mode="bilinear"))
+                    layer.append(nn.Conv2d(channels, channels // 2, 1))
                     channels = channels // 2
 
-            # channels = F * B * size
-            # todo dilation?
-            cells.append(
-                cell(channels, channels)
-            )
-            cells.append(activation())
+            self.cells.append(nn.Sequential(*layer).cuda())
 
-        cells.append(layers.ASPP(channels, 256, (6, 12, 18), (6, 12, 18)))
-        channels = 256
+        # Pool, then reduce channels to the desired value
+        self.pool = nn.Sequential(
+            layers.ASPP(channels, 256, (6, 12, 18), (6, 12, 18)),
+            nn.Conv2d(256, out_channels, 3, padding=1)
+        ).cuda()
 
-        # Reduce channels to the desired value
-        cells.append(nn.Conv2d(channels, out_channels, 3, padding=1))
+        self.upsampler = nn.Upsample(scale_factor=2 ** layout[-1], mode="bilinear")
 
-        if upsample_at_end:
-            cells.append(nn.Upsample(scale_factor=2 ** layout[-1], mode="bilinear"))
-
-        self.network = nn.Sequential(*cells).cuda()
 
     def forward(self, x):
-        return self.network(x)
+        x = self.initial_stem(x)
+
+        # Run stem layers
+        prev_hs = [self.cells[0](x)]
+        prev_hs.append(self.cells[1](prev_hs[0]))
+
+        for i, layer in enumerate(self.cells, 2):
+            curr = layer(prev_hs[-1], prev_hs[-2])
+            prev_hs[-2] = prev_hs[-1]
+            prev_hs[-1] = curr
+
+        x = self.pool(prev_hs[-1])
+        if self.upsample_at_end:
+            x = self.upsampler(x)
+
+        return x
+
 
 if __name__ == '__main__':
-    layout = [0, 1, 2, 2, 2, 2, 3, 4, 3, 4, 4, 5, 5, 4, 3]
+    layout = [2, 2, 2, 2, 3, 4, 3, 4, 4, 5, 5, 4, 3]
     model = AutoDeeplab(3, 3, layout, layers.Cell)
     print(model)
+
